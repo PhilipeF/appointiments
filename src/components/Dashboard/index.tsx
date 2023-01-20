6e9b3fcf35f6e75f1582af2b5e5e54fffe7942f2
rick <philipesferreiraa@gmail.com>
2023-01-20 12:35:58 -0300

    renomenando nome do component

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import { Summary } from "../Summary";
-import { TransactionTable } from "../Transactions";
+import { TransactionTable } from "../TransactionsTable";
 import { Content } from "./style";
 
 export function Dashboard() {
