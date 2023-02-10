b73149a66eb1c85299b03b6150aeabc2aa7da921
rick <philipesferreiraa@gmail.com>
2023-02-10 09:26:58 -0300

    adicionando o README na aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
-import { TransactionContext, TransactionProvider } from "./TransactionContext";
+import { TransactionProvider } from "./TransactionContext";
+
 import { GlobalStyle } from "./styles/global";
+import ReactModal from "react-modal";
+
 
 export function App() {
 
